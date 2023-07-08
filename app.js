5d0a2ec50b011d95c8affeee42b6922fb5a66f74
Victoria Mulhall <vicky.mulhall84@gmail.com>
2023-07-07 23:27:49 -0300

    inicio de css

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const app= express();
 const path = require('path');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-});
-
-
 
 app.listen('3000',()=>{
     console.log('Servidor escuchado en puerto:http://localhost:3000/')
 })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
