18c4860e68f0858e77b1ed4121c851ac6027b862
Victoria Mulhall <vicky.mulhall84@gmail.com>
2023-07-18 21:50:11 -0300

    Inicio ejercicio clase 21

--- app.js
+++ app.js
@@ -11,3 +11,9 @@ app.listen('3000',()=>{
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
